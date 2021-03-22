fff918c7c46e660d99307178b5654cbbbd106ac1
Rasha Alahmad <47429639+Rashama@users.noreply.github.com>
2021-03-22 09:38:59 -0400

    Update video.js

--- js/video.js
+++ js/video.js
@@ -9,7 +9,6 @@ window.addEventListener("load", function() {
 
 document.querySelector("#play").addEventListener("click", function() {
 console.log("Play Video");
-volume.innerHTML ="100%";
 video.play();
  });
 
